e7fc53122ffac84a7f189d172de4aa0efa8b0d67
iahuang <ianhuang@Bag-End.local>
2017-05-05 22:11:45 -0400

    Yay

--- AdvJam2017/Assets/Platformer.cs
+++ AdvJam2017/Assets/Platformer.cs
@@ -2,22 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Platformer : MonoBehaviour {
-	bool touching = false;
-	Vector3 grav = new Vector3(0, -.30625f, 0);
-	Vector3 velocity = new Vector3();
+public class Platformer : MonoBehaviour {	
 	// Use this for initialization
+	Rigidbody2D rb;
+	float rotAngle = 0.0f;
+	int rotMode = 0;
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position += velocity;
-		velocity += grav;
+		/* Rot modes:
+		 * -2 - Above y=0 rotating
+		 * -1 - Below y=0 static
+		 * 0  - Above y=0 static
+		 * 1  - Below y=0 rotating
+		 * 
+		*/
+		if (transform.position.y<0) {
+			rb.gravityScale = -1;
+			if (rotMode == 0) {
+				rotMode = 1;
+			}
+			if (rotMode == 1) {
+				float rotAmount = 360 * Time.deltaTime;
+				transform.Rotate (new Vector3 (0, 0, rotAmount));
+				rotAngle += rotAmount;
+				if (rotAngle >= 180) {
+					rotMode = -1;
+					rotAngle = 0;
+				}
+			}
+
+		} else {
+			rb.gravityScale = 1;
+			if (rotMode == -1) {
+				rotMode = -2;
+			}
+			if (rotMode == -2) {
+				float rotAmount = 360 * Time.deltaTime;
+				transform.Rotate (new Vector3 (0, 0, rotAmount));
+				rotAngle += rotAmount;
+				if (rotAngle >= 180) {
+					rotMode = 0;
+					rotAngle = 0;
+				}
+			}
+		}
 	}
-	void onCollisionEnter2D (Collider2D coll) {
-		print ("We hit something");
+	void OnCollisionEnter2D (Collision2D col) {
+		
 	}
 		
 }
