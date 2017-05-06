e574b175860f487b2899ecba98139e4971bab1a1
Meculus (Gold Rebirth) <ethan.saff@gmail.com>
2017-05-05 21:46:15 -0400

    Commit blocks the way

--- AdvJam2017/Assets/Platformer.cs
+++ AdvJam2017/Assets/Platformer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Platformer : MonoBehaviour {
 	bool touching = false;
 	Vector3 grav = new Vector3(0, -.30625f, 0);
+	Vector3 velocity = new Vector3();
 	// Use this for initialization
 	void Start () {
 		
@@ -12,7 +13,8 @@ public class Platformer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.position += velocity;
+		velocity += grav;
 	}
 	void onCollisionEnter2D (Collider2D coll) {
 		print ("We hit something");
