5b0448e05138bb22163577aba5dd9b5e940cc02a
Meculus (Gold Rebirth) <ethan.saff@gmail.com>
2017-05-05 21:19:05 -0400

    We got a commit

---
+++ AdvJam2017/Assets/Mirror.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour {
+	int recharge=10;
+	int t=0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t += 1;
+	}
+	void onTriggerEnter2D(Collider collider) {
+		if (t > recharge) {
+			t = 0;
+			Physics.gravity *= -1;
+		}
+	}
+
+}
